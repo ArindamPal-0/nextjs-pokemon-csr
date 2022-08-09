6b685a4c27e0af81cc1c8c34fbc5a0f66dad9196
Arindam Pal <arindampal2000@gmail.com>
2022-08-09 21:13:28 +0530

    proper display of the pokemon details page

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,4 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 import { useState, useEffect } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../../styles/Details.module.css";
 import { useRouter } from "next/router";
 
 interface Stat {
@@ -33,5 +37,45 @@ export default function Details() {
     }
   }, [id]);
 
-  return <div>{JSON.stringify(pokemon)}</div>;
+  return (
+    <div>
+      <Head>
+        <title>{pokemon?.name}</title>
+      </Head>
+      <div>
+        <Link href="/">
+          <a>Back to Home</a>
+        </Link>
+        <div className={styles.layout}>
+          <div>
+            <img
+              className={styles.picture}
+              src={`https://arindampal-0.github.io/pokemon-api/${pokemon?.image}`}
+              alt={pokemon?.name}
+            />
+          </div>
+          <div>
+            <div className={styles.name}>{pokemon?.name}</div>
+            <div className={styles.type}>{pokemon?.type.join(", ")}</div>
+            <table>
+              <thead className={styles.header}>
+                <tr>
+                  <th>Name</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                {pokemon?.stats.map(({ name, value }: Stat) => (
+                  <tr key={name}>
+                    <td className={styles.attribute}>{name}</td>
+                    <td>{value}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
